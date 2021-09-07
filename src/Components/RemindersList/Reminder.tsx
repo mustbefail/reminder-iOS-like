cc2b86c7480f2b0bf8a770afda62a25971eac467
Viktor Kushnir <mustbefail@gmail.com>
2021-09-07 23:04:21 +0300

    refactor components, add redux

--- src/Components/RemindersList/Reminder.tsx
+++ src/Components/RemindersList/Reminder.tsx
@@ -1,12 +1,35 @@
 import { FC } from 'react';
-import { IReminder } from '../../types/types';
+import { IReminder } from '../../models/types';
+import cn from 'classnames';
+
+import styles from './Reminder.module.scss';
+import { useDispatch } from 'react-redux';
+import { ReminderActionCreators } from '../../store/reducers/reminders/action-creators';
 
 const Reminder: FC<{ reminder: IReminder }> = ({ reminder }) => {
-  const { reminderText } = reminder;
+  const { text, completed, id } = reminder;
+  const dispatch = useDispatch();
+
+  const iconClassnames = {
+    fas: completed,
+    'text-orange': completed,
+    'fa-circle': true,
+    far: !completed,
+  };
+
+  const completeReminderHandler = () => {
+    dispatch(ReminderActionCreators.SetReminderComplete(!completed, id));
+  };
+
   return (
-    <li>
-      <i className='far fa-circle'></i>
-      <span>{reminderText}</span>
+    <li className={styles.reminder}>
+      <span
+        className={styles['reminder-icon']}
+        onClick={completeReminderHandler}
+      >
+        <i className={cn(iconClassnames)}></i>
+      </span>
+      <span className={styles['reminder-text']}>{text}</span>
     </li>
   );
 };
--- src/Components/RemindersList/ReminderList.tsx
+++ src/Components/RemindersList/ReminderList.tsx
@@ -1,15 +1,34 @@
-import { FC } from 'react';
-import { reminders } from '../../fixtures/mockups';
+import { FC, useEffect } from 'react';
 import Reminder from './Reminder';
 import { uniqueId } from 'lodash';
 
+import styles from './ReminderList.module.scss';
+
+import { useTypedSelector } from '../../hooks/useTypedSelector';
+import AddReminderForm from './AddReminderForm';
+import { useDispatch } from 'react-redux';
+import { ReminderActionCreators } from '../../store/reducers/reminders/action-creators';
+
 const ReminderList: FC = () => {
+  const reminders = useTypedSelector((state) => state.reminders.reminders);
+  const dispatch = useDispatch();
+  const isFormVisible = useTypedSelector(
+    (state) => state.uiReducer.isFormVisible
+  );
+
+  useEffect(() => {
+    dispatch(ReminderActionCreators.loadReminders());
+  }, []);
+
   return (
-    <ul>
-      {reminders.map((reminder) => (
-        <Reminder key={uniqueId()} reminder={reminder} />
-      ))}
-    </ul>
+    <>
+      <ul className={styles['reminder-list']}>
+        {reminders.map((reminder) => {
+          return <Reminder key={uniqueId()} reminder={reminder} />;
+        })}
+      </ul>
+      {isFormVisible && <AddReminderForm />}
+    </>
   );
 };
 
