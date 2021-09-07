a8ba86258e6ea6a66d9aa59138c29a4be95d8ce3
Viktor Kushnir <mustbefail@gmail.com>
2021-09-07 23:08:14 +0300

    add font awesome icons, global styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './store/index';
 
+import '@fortawesome/fontawesome-free/js/all';
+import './styles/global.scss';
 import './index.scss';
 
 const rootElement = document.getElementById('root');
