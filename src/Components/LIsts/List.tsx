39dda18b3a2cffa5bb7fd59eac49d28f82eff5fd
Viktor Kushnir <mustbefail@gmail.com>
2021-09-07 23:01:12 +0300

    add classname to el

--- src/Components/LIsts/List.tsx
+++ src/Components/LIsts/List.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { IList } from '../../types/types';
+import { IList } from '../../models/types';
 import cn from 'classnames';
 
 import styles from './List.module.scss';
@@ -7,7 +7,7 @@ import styles from './List.module.scss';
 const List: FC<{ list: IList }> = ({ list }) => {
   const { name, amount, iconColor, iconName } = list;
   return (
-    <li>
+    <li className={styles.list}>
       <span className={cn('icon', `bg-${iconColor}`)}>
         <i className={iconName}></i>
       </span>
